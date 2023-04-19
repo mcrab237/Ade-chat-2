3b7736c4a1bfaacf39d8a9a733d7f2bd002e8e67
ato237 <tonybradpit@gmail.com>
2023-04-19 14:43:45 -0400

    lates

--- server/server.ts
+++ server/server.ts
@@ -8,7 +8,7 @@ import { userRoutes } from "./routes/users"
 const app = fastify({ logger: true })
 app.register(cors, { origin: (origin, cb) => {
     // Check if the origin is allowed
-    if (origin === 'http://127.0.0.1:5173' || origin === 'http://localhost:5173' || process.env.HEROKU_HOST) {
+    if (origin === 'http://127.0.0.1:5173' || origin === 'http://localhost:5173' || process.env.HEROKU_HOST || origin === "https://6440282ebea36e11a3e7e589--gentle-tulumba-6220c9.netlify.app/") {
       cb(null, true);
     } else {
       cb(new Error('Not allowed by CORS'), false);
